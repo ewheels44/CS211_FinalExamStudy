0c89e7b50676e06a8e18e17a4259f2d8424aa554
Ethan Wheeler <ethanwheeler@Ethans-MBP.home>
2021-06-10 20:56:08 -0500

    first commit

---
+++ tesingconcepts.cpp
@@ -0,0 +1,89 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h> 
+
+using namespace std;	// Debatable, but easier for now.
+
+class  Myclass{
+
+	private:
+		int Values;
+
+	public:
+		Myclass(); 										// No argument constructor
+		Myclass(const Myclass & Other); 				// Copy constructor
+		Myclass &operator=(const Myclass & Other); 		// assignment operator
+		virtual ~Myclass(); 							// class destructor
+
+
+		//methods
+		void dosomething(int x);
+};
+
+// original (no edits)
+/* void myfunc( int A, int * B, int C[ ], int * D ) { */
+/*  */
+/*  */
+/*      A = 42; */
+/*  */
+/*      *B = 42; */
+/*  */
+/*      C[ 0 ] = 42; */
+/*  */
+/*      D = 42; */
+/*  */
+/*  */
+/*      return; */
+/*  */
+/* } */
+
+// writen for C
+// fixed function from above
+//
+void myfunc( int A, int * B, int C[ ], int * D ) {
+
+
+     A = 42;
+
+     *B = 42;
+
+     C[ 0 ] = 42;
+
+     *D = 42;
+
+     return;
+
+}
+
+void myfunc2( int A, int *B, int C[], int &D)
+{
+	A = 42;
+	*B = 42;
+	C[0] = 42;
+	D = 42;
+}
+
+//A function that takes a douuble as an arugment and returns an int
+
+
+int main(int argc, const char** argv) {
+
+	int A, B, C, D = 0;
+
+	cout << "original numbers" << endl;
+	cout << A << " " << B << " " << C << " " << D << endl;
+
+	myfunc(A, &B, &C, &D);
+
+	cout << "myfunc" << endl;
+	cout << A << " " << B << " " << C << " " << D << endl;
+
+	myfunc2(A, &B, &C, D);
+
+	cout << "myfunc2" << endl;
+	cout << A << " " << B << " " << C << " " << D << endl;
+
+    return 0;
+}
